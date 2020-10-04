6029c3f31adc12937bdfcd11d03b5578b4e9a5b9
Esteban Cervera <cerverae18@hotmail.com>
2020-10-04 17:19:25 -0500

    First Commit

---
+++ src/main/java/com/mayab/calidad/doubles/tareaUnitTest/App.java
@@ -0,0 +1,13 @@
+package com.mayab.calidad.doubles.tareaUnitTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
